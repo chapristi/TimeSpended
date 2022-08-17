25e4b79eece9115d6067354c7dad25ce2e70e826
chapristi <70172942+chapristi@users.noreply.github.com>
2022-08-17 20:53:50 +0200

    Delete EventSubscriberChangeWindow.py

--- src/Event/EventSubscriberChangeWindow.py
+++
@@ -1,9 +0,0 @@
-import time
-import pygetwindow as gw
-class EventSubscriberChangeWindow:
-    #i'm looking for a way to get event et delete this time.sleep
-    def whatWindow():
-        while True:
-            time.sleep(5)
-            print(gw.getActiveWindow().title)
-    
