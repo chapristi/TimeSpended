70e6114af769f702e0605b5478ea0f2aea71a47c
chapristi <70172942+chapristi@users.noreply.github.com>
2022-08-17 20:59:44 +0200

    typage

--- src/Service/File.py
+++ src/Service/File.py
@@ -3,10 +3,10 @@ import string
 class File:
     def __init__(self,file: string) -> None:
         self.file = file
-    def readFile(self):
+    def readFile(self: any) -> string:
         with open('../../infos.txt','r') as file_data:
             return file_data.read()
-    def writeFile(self,app: string):
+    def writeFile(self: any,app: string) -> any:
         time = datetime.now()
         object = f"{app}_{str(time)}\n_"
         with open(self.file, "a") as fichier:
