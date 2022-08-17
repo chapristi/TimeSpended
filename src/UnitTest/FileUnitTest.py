8c14bbeb164c37fb48a2f77944367ad269511275
chapristi <70172942+chapristi@users.noreply.github.com>
2022-08-17 20:52:24 +0200

    ajout des tests

--- src/UnitTest/FileUnitTest.py
+++ src/UnitTest/FileUnitTest.py
@@ -1,8 +1,15 @@
 import unittest
-
+TESTDATA_FILENAME = "../../infos.txt"
+'''
+class to test the code in src/Service/File.py 
+'''
 class FileUnitTest(unittest.TestCase):
-    def testFileExist(self):
-        fr = open('../infos.json','r')
-        self.assertIsNotNone(fr.read())
+    #test the file to be opened and read to ensure that it works properly
+   def testRead(self):
+       self.testfile = open(TESTDATA_FILENAME)
+       self.testdata = self.testfile.read()
+   def tearDown(self):
+       self.testfile.close()
+
         
 
